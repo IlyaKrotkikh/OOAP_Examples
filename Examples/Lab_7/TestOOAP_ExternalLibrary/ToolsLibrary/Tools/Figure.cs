baf5b32999829168044da84a4e95b28cd3ed9fe0
Илья Кротких <ivashca99@gmail.com>
2018-03-04 22:52:57 +0300

    Added class files, renamed library project.

---
+++ Examples/Lab_7/TestOOAP_ExternalLibrary/ToolsLibrary/Tools/Figure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolsLibrary
+{
+    /// <summary>
+    /// Абстрактный класс Figure (Фигура), наследуется от абстрактного класса DrawTool
+    /// </summary>
+    abstract class Figure : DrawTool
+    {
+        private int _Height, _Width;
+
+        /// <summary>
+        /// Поле, содержит данные о высоте фигуры.
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return this._Height;
+            }
+            set
+            {
+                this._Height = value;
+            }
+        }
+        /// <summary>
+        /// Поле, содержит информацию о ширине фигуры.
+        /// </summary>
+        public int Width
+        {
+            get
+            {
+                return this._Width;
+            }
+            set
+            {
+                this._Width = value;
+            }
+        }
+    }
+}
---
+++ Examples/Lab_7/TestOOAP_ExternalLibrary/ToolsLibrary/Tools/Interfaces/IFilled.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolsLibrary
+{
+    /// <summary>
+    /// Интерфейс. Требует от реализующего класса реализацию метода Fill(string, OutputHandler)
+    /// </summary>
+    public interface IFilled
+    {
+        /// <summary>
+        /// Предполагает реализацию заливки фигуры цветом.
+        /// </summary>
+        /// <param name="Color">Цвет заливки.</param>
+        /// <param name="ToolsOutputHandler">Контроллер вывода данных.</param>
+        void Fill(string Color, OutputHandler ToolsOutputHandler);
+    }
+}
